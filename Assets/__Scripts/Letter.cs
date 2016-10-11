595cce30d674ff4ed0ca6dcf48a96afc7d17873f
mplawley <mark.lawley@gmail.com>
2016-10-11 15:14:57 -0400

    Core functionality for laying out the letters

---
+++ Assets/__Scripts/Letter.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Letter : MonoBehaviour
+{
+	private char _c; //The char shown on this letter
+	public TextMesh tMesh; //The TextMesh shows the char
+	public Renderer tRend; //The Renderer of the 3D Text. This will determine whether the char is visible
+	public bool big = false; //Big letters act differently
+
+	void Awake()
+	{
+		tMesh = GetComponentInChildren<TextMesh>();
+		tRend = tMesh.GetComponent<Renderer>();
+		visible = false;
+	}
+
+	//Used to get or set _c and the letter shown by 3D Text
+	public char c
+	{
+		get
+		{
+			return _c;
+		}
+		set
+		{
+			_c = value;
+			tMesh.text = _c.ToString();
+		}
+	}
+
+	//Gets or sets _c as a strin
+	public string str
+	{
+		get
+		{
+			return _c.ToString();
+		}
+		set
+		{
+			c = value[0];
+		}
+	}
+
+	//Enables or disables the renderer for 3D Text, which causes the char to be visible or invisible respectively
+	public bool visible
+	{
+		get
+		{
+			return tRend.enabled;
+		}
+		set
+		{
+			tRend.enabled = value;
+		}
+	}
+
+	//Gets or sets the color of the rounded rectangle
+	public Color color
+	{
+		get
+		{
+			return GetComponent<Renderer>().material.color;
+		}
+		set
+		{
+			GetComponent<Renderer>().material.color = value;
+		}
+	}
+
+	//Sets the position of the Letter's gameObject
+	public Vector3 pos
+	{
+		set
+		{
+			transform.position = value;
+		}
+	}
+}
--- Assets/__Scripts/WordGame.cs
+++ Assets/__Scripts/WordGame.cs
@@ -15,11 +15,22 @@ public enum GameMode
 public class WordGame : MonoBehaviour
 {
 	public static WordGame S; //Singleton
+	public GameObject prefabLetter;
+	public Rect wordArea = new Rect(-24, 19, 48, 28);
+	public float letterSize = 1.5f;
+	public bool showAllWyrds = true;
+	public float bigLetterSize = 4f;
+	public Color bigColorDim = new Color(0.8f, 0.8f, 0.8f);
+	public Color bigColorSelected = Color.white;
+	public Vector3 bigLetterCenter = new Vector3(0, -16, 0);
 
 	[Header("------------")]
 
 	public GameMode mode = GameMode.preGame;
 	public WordLevel currLevel;
+	public List<Wyrd> wyrds;
+	public List<Letter> bigLetters;
+	public List<Letter> bigLettersActive;
 
 	void Awake()
 	{
@@ -121,5 +132,140 @@ public class WordGame : MonoBehaviour
 	public void SubWordSearchComplete()
 	{
 		mode = GameMode.levelPrep;
+		Layout();
+	}
+
+	void Layout()
+	{
+		//Place the letters for each subword of currLevel on screen
+		wyrds = new List<Wyrd>();
+
+		//Variables that will be used in this method
+		GameObject go;
+		Letter lett;
+		string word;
+		Vector3 pos;
+		float left = 0;
+		float columnWidth = 3;
+		char c;
+		Color col;
+		Wyrd wyrd;
+
+		//Determine how many rows of Letters will fit on screen
+		int numRows = Mathf.RoundToInt(wordArea.height / letterSize);
+
+		//Make a Wyrd of each level.subWord
+		for (int i = 0; i < currLevel.subWords.Count; i++)
+		{
+			wyrd = new Wyrd();
+			word = currLevel.subWords[i];
+
+			//If the word is longer than columnWidth, expand it
+			columnWidth = Mathf.Max(columnWidth, word.Length);
+
+			//Instantiate a PrefabLetter for each letter of the word
+			for (int j = 0; j < word.Length; j++)
+			{
+				c = word[j]; //Grab the jth char of the word
+				go = Instantiate(prefabLetter) as GameObject;
+				lett = go.GetComponent<Letter>();
+				lett.c = c; //Set the c of the Letter
+
+				//Position the Letter
+				pos = new Vector3(wordArea.x + left + j * letterSize, wordArea.y, 0);
+
+				//The % here makes multiple columns line up
+				pos.y -= (i % numRows) * letterSize;
+				lett.pos = pos;
+				go.transform.localScale = Vector3.one * letterSize;
+				wyrd.Add(lett);
+			}
+
+			if (showAllWyrds)
+			{
+				wyrd.visible = true; //Line for testing
+			}
+
+			wyrds.Add(wyrd);
+
+			//If we've gotten to the numRows(th) row, start a new column
+			if (i % numRows == numRows - 1)
+			{
+				left += (columnWidth + 0.5f) * letterSize;
+			}
+		}
+
+		//Place the big letters
+		//Initialize the List<>s for big Letters
+		bigLetters = new List<Letter>();
+		bigLettersActive = new List<Letter>();
+
+		//Create a big Letter for each letter in the target word
+		for (int i = 0; i < currLevel.word.Length; i++)
+		{
+			//This is similar to the process for a normal Letter
+			c = currLevel.word[i];
+			go = Instantiate(prefabLetter) as GameObject;
+			lett = go.GetComponent<Letter>();
+			lett.c = c;
+			go.transform.localScale = Vector3.one * bigLetterSize;
+
+			//Set the initial position of the big Letters below the screen
+			pos = new Vector3 (0, -100, 0);
+			lett.pos = pos;
+
+			col = bigColorDim;
+			lett.color = col;
+			lett.visible = true; //This is always true for big letters
+			lett.big = true;
+			bigLetters.Add(lett);
+		}
+
+		//Shuffle the big letters
+		bigLetters = ShuffleLetters(bigLetters);
+
+		//Arrange them on screen
+		ArrangeBigLetters();
+
+		//Set the mode to be in-game
+		mode = GameMode.inLevel;
+	}
+
+	//This shuffles a List<Letter> randomly and returns the result
+	List<Letter> ShuffleLetters(List<Letter> letts)
+	{
+		List<Letter> newL = new List<Letter>();
+		int ndx;
+		while (letts.Count > 0)
+		{
+			ndx = Random.Range(0, letts.Count);
+			newL.Add(letts[ndx]);
+			letts.RemoveAt(ndx);
+		}
+		return newL;
+	}
+
+	//This arranges the big Letters on screen
+	void ArrangeBigLetters()
+	{
+		//The halfWidth allows the big Letters to be centered
+		float halfWidth = ((float) bigLetters.Count) / 2f - 0.5f;
+		Vector3 pos;
+		for (int i = 0; i < bigLetters.Count; i++)
+		{
+			pos = bigLetterCenter;
+			pos.x += (i - halfWidth) * bigLetterSize;
+			bigLetters[i].pos = pos;
+		}
+
+		//bigLettersActive
+		halfWidth = ((float) bigLettersActive.Count) / 2f - 0.5f;
+		for (int i = 0; i < bigLettersActive.Count; i++)
+		{
+			pos = bigLetterCenter;
+			pos.x += (i - halfWidth) * bigLetterSize;
+			pos.y += bigLetterSize * 1.25f;
+			bigLettersActive[i].pos = pos;
+		}
 	}
 }
